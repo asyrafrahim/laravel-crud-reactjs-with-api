55293db0ca6a7052a5daaf06528884a26499fbd3
Asyraf Rahim <asyrafboyz@gmail.com>
2022-03-03 11:35:59 +0800

    changed route resources to route get, post

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,10 +18,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('products', ProductController::class);
-
-Route::get('/products', 'API\ProductController@index')->name('api:product:index');
-Route::post('/products/create', 'API\ProductController@store')->name('api:product:store');
-Route::get('/products/{product}', 'API\ProductController@show')->name('api:product:show');
-Route::put('/products/{product}', 'API\ProductController@update')->name('api:product:update');
-Route::delete('/products/{product}', 'API\ProductController@destroy')->name('api:product:destroy');
+Route::get('/products', 'API\ProductController@index')->name('products.index');
+Route::post('/products', 'API\ProductController@store')->name('products.store');
+Route::get('/products/{product}', 'API\ProductController@show')->name('products.show');
+Route::patch('/products/{product}', 'API\ProductController@update')->name('products.update');
+Route::delete('/products/{product}', 'API\ProductController@destroy')->name('products.destroy');
